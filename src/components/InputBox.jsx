f2c086288c8cdf0da57f37d62e8d63aec267a8e5
Shivani Singh <91026976+ShivaniiSinghh@users.noreply.github.com>
2024-09-10 03:17:09 +0530

    Add files via upload

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,62 @@
+import React, {useId} from 'react'
+
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",
+}) {
+   const amountInputId = useId()
+
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
+                    {label}
+                </label>
+                <input
+                    id={amountInputId}
+                    className="outline-none w-full bg-transparent py-1.5"
+                    // adding transparency in to the currency card for adding more depth to the convertor card
+                    type="text"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                    // same is happening here as described below about the onChange event, here , we are changing and updating the value
+                    // of currency like which amount of currency change we want to detect
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    // onChange is an event handler, that too a [Synthetic Event] using Form event to handle the change coming on onclick
+                    // event is happening on onChange event and it has target, whose value we are taking in to the consideration
+                    // here, we are getting the event of changing the type of currency, and onChange is handling this event updation
+                    // so, we can change the inr to usd or vv or any type of currency to get the required amount value and conversion
+
+                    disabled={currencyDisable}
+                >
+                    
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
+                            {currency}
+                            </option>
+                        ))}
+                
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default InputBox;
---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import {useEffect, useState} from "react"
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+        // We are using the currency API here for fetching all the required type of currency we want to get information and conversion about.
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
